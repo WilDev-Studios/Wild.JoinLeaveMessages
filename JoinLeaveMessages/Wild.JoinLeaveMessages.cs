2a6681d7379d08911c640a56f0357da8c8117b07
WildKadeGaming <91111094+WildKadeGaming@users.noreply.github.com>
2021-10-20 21:57:21 -0600

    v1.2.1

--- JoinLeaveMessages/Wild.JoinLeaveMessages.cs
+++ JoinLeaveMessages/Wild.JoinLeaveMessages.cs
@@ -7,11 +7,11 @@ using OpenMod.Unturned.Plugins;
 using OpenMod.API.Plugins;
 using OpenMod.API.Eventing;
 using System.Threading.Tasks;
-using OpenMod.API.Users;
 using OpenMod.Unturned.Players.Connections.Events;
 using OpenMod.Core.Users.Events;
 using OpenMod.Core.Eventing;
 using System.Collections.Generic;
+using SDG.Unturned;
 
 [assembly: PluginMetadata("Wild.JoinLeaveMessages", DisplayName = "Wild.JoinLeaveMessages", Author = "WilDev Studios", Website = "https://discord.gg/4Ggybyy87d")]
 namespace JoinLeaveMessages
@@ -54,23 +54,20 @@ namespace JoinLeaveMessages
     public class UserFirstConnectionHandler : IEventListener<IUserFirstConnectingEvent>
     {
         private readonly IConfiguration m_Configuration;
-        private readonly IUserManager m_UserManager;
         private readonly IStringLocalizer m_StringLocalizer;
 
         public static List<string> ConnectingMembers = new List<string>();
 
         public UserFirstConnectionHandler(
             IConfiguration configuration,
-            IUserManager userManager,
             IStringLocalizer stringLocalizer)
         {
             m_Configuration = configuration;
-            m_UserManager = userManager;
             m_StringLocalizer = stringLocalizer;
         }
 
         [EventListener(Priority = EventListenerPriority.Lowest)]
-        public async Task HandleEventAsync(object sender, IUserFirstConnectingEvent @event)
+        public Task HandleEventAsync(object sender, IUserFirstConnectingEvent @event)
         {
             var user = @event.User.DisplayName;
             var first_join_enabled = m_Configuration.GetSection("Values:First-Join-Enabled").Get<bool>();
@@ -78,29 +75,28 @@ namespace JoinLeaveMessages
             if (first_join_enabled == true)
             {
                 ConnectingMembers.Add(user);
-                await m_UserManager.BroadcastAsync(m_StringLocalizer["Messages:First-Join-Message", new { Player = user }]);
+                ChatManager.serverSendMessage(m_StringLocalizer["Messages:First-Join-Message", new { Player = user }], UnityEngine.Color.green, null, null, EChatMode.GLOBAL, m_Configuration.GetSection("First-Join-Image-URL").Get<string>(), true);
             }
+
+            return Task.CompletedTask;
         }
     }
 
     public class UserConnectionHandler : IEventListener<UnturnedPlayerConnectedEvent>
     {
         private readonly IConfiguration m_Configuration;
-        private readonly IUserManager m_UserManager;
         private readonly IStringLocalizer m_StringLocalizer;
 
         public UserConnectionHandler(
             IConfiguration configuration,
-            IUserManager userManager,
             IStringLocalizer stringLocalizer)
         {
             m_Configuration = configuration;
-            m_UserManager = userManager;
             m_StringLocalizer = stringLocalizer;
         }
 
         [EventListener(Priority = EventListenerPriority.Low)]
-        public async Task HandleEventAsync(object sender, UnturnedPlayerConnectedEvent @event)
+        public Task HandleEventAsync(object sender, UnturnedPlayerConnectedEvent @event)
         {
             var user = @event.Player;
             var stringUser = user.ToString();
@@ -113,46 +109,46 @@ namespace JoinLeaveMessages
                 {
                     if (UserFirstConnectionHandler.ConnectingMembers[i] == stringUser)
                     {
-                        await m_UserManager.BroadcastAsync(m_StringLocalizer["Messages:First-Join-Message", new { Player = user }]);
                         UserFirstConnectionHandler.ConnectingMembers.Remove(stringUser);
-                        return;
+                        return Task.CompletedTask;
                     }
                 }
             }
 
             if (join_enabled == true)
             {
-                await m_UserManager.BroadcastAsync(m_StringLocalizer["Messages:Join-Message", new { Player = user }]);
+                ChatManager.serverSendMessage(m_StringLocalizer["Messages:Join-Message", new { Player = user }], UnityEngine.Color.green, null, null, EChatMode.GLOBAL, m_Configuration.GetSection("Join-Image-URL").Get<string>(), true);
             }
+
+            return Task.CompletedTask;
         }
     }
 
     public class UserDisconnectionHandler : IEventListener<UnturnedPlayerDisconnectedEvent>
     {
         private readonly IConfiguration m_Configuration;
-        private readonly IUserManager m_UserManager;
         private readonly IStringLocalizer m_StringLocalizer;
 
         public UserDisconnectionHandler(
             IConfiguration configuration,
-            IUserManager userManager,
             IStringLocalizer stringLocalizer)
         {
             m_Configuration = configuration;
-            m_UserManager = userManager;
             m_StringLocalizer = stringLocalizer;
         }
 
         [EventListener(Priority = EventListenerPriority.Low)]
-        public async Task HandleEventAsync(object sender, UnturnedPlayerDisconnectedEvent @event)
+        public Task HandleEventAsync(object sender, UnturnedPlayerDisconnectedEvent @event)
         {
             var user = @event.Player;
             var leave_enabled = m_Configuration.GetSection("Values:Leave-Enabled").Get<bool>();
 
             if (leave_enabled == true)
             {
-                await m_UserManager.BroadcastAsync(m_StringLocalizer["Messages:Leave-Message", new { Player = user }]);
+                ChatManager.serverSendMessage(m_StringLocalizer["Messages:Leave-Message", new { Player = user }], UnityEngine.Color.green, null, null, EChatMode.GLOBAL, m_Configuration.GetSection("Leave-Image-URL").Get<string>(), true);
             }
+
+            return Task.CompletedTask;
         }
     }
 }
